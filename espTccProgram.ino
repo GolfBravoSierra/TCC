ed8a53b34f014960f3d2bfe99f41af281fc47c57
Giovani BS <106974254+GolfBravoSierra@users.noreply.github.com>
2026-04-21 16:54:12 -0300

    Refactor medication scheduling and update WiFi credentials
    Updated WiFi credentials and refactored medication scheduling logic. Improved program handling for daily medication reminders.

--- espTccProgram.ino
+++ espTccProgram.ino
@@ -25,8 +25,8 @@ Stepper myStepper(stepsPerRevolution, IN1, IN3, IN2, IN4);
 //Keypad keyBoard = Keypad(makeKeymap(keys), linePins, columnsPins, lines, columns);
 //String keyboardInput = "";
 
-const char* ssid = "Pixel7Giovani";
-const char* password = "naotemsenha";
+const char* ssid = "SANTOS"; //Pixel7Giovani
+const char* password = "santos374"; //naotemsenha
 const char* ntpServer = "pool.ntp.org";
 const long  brasiliaTime = -3 * 3600;
 const int   summerTimeOffset = 0;
@@ -40,20 +40,21 @@ const float storageCarouselSlots[21] = {
 };
 
 struct medicineTimeAndSlot {
-  int weekDay
+  int weekDay;
   int hour;
   int minutes; 
   int slot;
 };
 
-medicineTimeAndSlot medsCalender[21];
+medicineTimeAndSlot medsProgramCalender[21];
+int medsProgramTotalCount = 0;
 
 bool isNotSunday(){
   struct tm currentTime;
-
   if (!getLocalTime(&currentTime)) {
     return false; 
   } 
+
   if (currentTime.tm_wday != 0) {
     return true; 
   } else {
@@ -64,14 +65,116 @@ bool isNotSunday(){
 
 bool haveProgramToday(){
   struct tm currentTime;
-  for(int i=0; i > 21 ;i++){
-    if(currentTime.tm_wday == medsCalender[i].weekDay{
-      if(medsCalender[i].hour != 0){
-        return true;
-      }
+  if (!getLocalTime(&currentTime)) {
+    return false; 
+  } 
+
+  for(int i=0; i < 21 ;i++){
+    if(currentTime.tm_wday == medsProgramCalender[i].weekDay){
+      return true;
     }
   }
   return false;
+}
+
+bool isTimeForMeds(){
+  struct tm currentTime;
+  if (!getLocalTime(&currentTime)) {
+    return 0; 
+  } 
+
+  Serial.println("vendo se e hora de remedio");
+  for(int i=0; i < 21; i++){
+    if(currentTime.tm_hour == medsProgramCalender[i].hour){
+      return true;
+      Serial.println("sim he hora do remedio");
+    }
+  }
+  return false;
+}
+
+void makeNewProgram(){
+  Serial.println("Digite qual compartimento deseja programar:(1 a 21)\n");
+  while (Serial.available() == 0){ delay(10); }
+  int selectedSlot = Serial.parseInt();
+  while (Serial.available() > 0) {
+  Serial.read();
+  }
+
+  Serial.println("digite a hora do remedio:\n");
+  while (Serial.available() == 0){ delay(10); }
+  medsProgramCalender[selectedSlot].hour = Serial.parseInt();
+  while (Serial.available() > 0) {
+  Serial.read();
+  }
+
+  Serial.printf("digite os minutos:\n");
+  while (Serial.available() == 0){ delay(10); }
+  medsProgramCalender[selectedSlot].minutes = Serial.parseInt();
+  while (Serial.available() > 0) {
+  Serial.read();
+  }
+
+  Serial.println("digite o dia do remedio:\n");
+  while (Serial.available() == 0){ delay(10); }
+  medsProgramCalender[selectedSlot].weekDay = Serial.parseInt();
+  while (Serial.available() > 0) {
+    Serial.read();
+  }
+
+  Serial.printf("programacao salva: Slot %02d, Horario %02d:%02d, Dia %02d.\n", selectedSlot, medsProgramCalender[selectedSlot].hour, medsProgramCalender[selectedSlot].minutes, medsProgramCalender[selectedSlot].weekDay);
+
+}
+
+void sleepyDayMode(){
+
+  if (haveProgramToday() == true) {
+    return; 
+  }
+
+  Serial.println("\n--- MODO SLEEPY DAY ATIVADO ---");
+  Serial.println("Nenhum remedio para hoje. Aguardando a meia-noite...");
+
+  while (true) {
+    
+    // Atualiza o relógio
+    if (!getLocalTime(&currentTime)) {
+      delay(1000);
+      return 0 
+    }
+
+    if (currentTime.tm_hour == 0 && currentTime.tm_min == 0) {
+      Serial.println("\nMeia-noite! Acordando para um novo dia...");
+      break;
+    }
+
+    if (currentTime.tm_min == 0 && currentTime.tm_sec < 5) {
+      Serial.printf("Zzz... Sleepy Mode... %02d:%02d\n", currentTime.tm_hour, currentTime.tm_min);
+    }
+
+    delay(30000);
+  }
+}
+
+int findTodayPrograms(int* foudedProgramsFoToday){
+   
+  struct tm currentTime;
+  if (!getLocalTime(&currentTime)) {
+    delay(1000);
+    return 0
+  }
+
+  int foundedProgramsForToday[3]; 
+  int counter = 0;
+
+  for(int programNumber = 0; programNumber < 21; programNumber++){
+    if(currentTime.tm_wday == medsProgramCalender[programNumber].weekDay){
+      foundedProgramsForToday[counter] = programNumber;
+      counter ++;
+    }
+  }
+
+  return couter;
 }
 
 void setup() {
@@ -85,27 +188,34 @@ void setup() {
   }
   Serial.println("\nWifi -> OK");
   configTime(brasiliaTime, summerTimeOffset, ntpServer);
+
   
   myStepper.setSpeed(5);
+
+  makeNewProgram();
 
 }
 
 
 void loop() {
   struct tm currentTime;
-  if(!getLocaTime($currentTime)){
+  if(!getLocalTime(&currentTime)){
     return;
   }
+
+  sleepyDayMode();
+
+  Serial.printf("%02d:%02d:%02d\n", currentTime.tm_hour, currentTime.tm_min, currentTime.tm_sec);
   
-  if(isNotSunday){
-   if(haveProgramToday){
-    
-   }else{
-    // aqui vai ter um print no lcd dizendo que não programação para hoje
-   }
-  }   
-  else{
-    
+  if(isNotSunday() && haveProgramToday()){
+    int todayPrograms[3];
+    int programQuantityForToday = findTodayPrograms(todayPrograms);
+
+    if(isTimeForMeds()){
+      Serial.println("it is time for meds");
+      delay(10000);
+    }
   }
 
+  delay(10000);
 }
