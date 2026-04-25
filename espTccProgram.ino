3fb87c1487380c101efdad86c8bd4d7d03333278
Giovani BS <106974254+GolfBravoSierra@users.noreply.github.com>
2026-04-25 10:15:30 -0300

    Refactor medication scheduling and time handling

--- espTccProgram.ino
+++ espTccProgram.ino
@@ -2,13 +2,6 @@
 #include <time.h>
 #include <WiFi.h>
 
-#define IN1 19
-#define IN2 18
-#define IN3 5
-#define IN4 17
-
-const int stepsPerRevolution = 2048;
-Stepper myStepper(stepsPerRevolution, IN1, IN3, IN2, IN4);
 
 //const byte lines = 4;
 //const byte columns = 4;
@@ -44,12 +37,14 @@ struct medicineTimeAndSlot {
 medicineTimeAndSlot medsProgramCalender[21];
 int medsProgramTotalCount = 0;
 
+struct tm currentTime;
+
 bool isSunday(){
-  struct tm currentTime;
+  
   if (!getLocalTime(&currentTime)) {
     return false; 
   } 
-
+  
   if (currentTime.tm_wday == 0) {
     return true; 
   } else {
@@ -58,12 +53,12 @@ bool isSunday(){
   
 }
 
-bool HaveProgramsForToday(){
-  struct tm currentTime;
+bool thereAreProgramsForToday(){
+  
   if (!getLocalTime(&currentTime)) {
     return false; 
   } 
-
+  
   for(int i=0; i < 21 ;i++){
     if(currentTime.tm_wday == medsProgramCalender[i].weekDay){
       return true;
@@ -74,13 +69,13 @@ bool HaveProgramsForToday(){
 
 bool isTimeForMeds(){
 
-  struct tm currentTime;
+  
   if (!getLocalTime(&currentTime)) {
     return 0; 
   } 
 
   int theMedForThisHour;
-
+  
   if (currentTime.tm_min % 15 == 0){
     for(int i = 0; i < 21; i++){
       if(currentTime.tm_hour == medsProgramCalender[i].hour && currentTime.tm_min == medsProgramCalender[i].minutes){
@@ -88,124 +83,171 @@ bool isTimeForMeds(){
       }
     }
   }
-
+  return false;
 }
 
 void makeNewProgram(){
-
+  
   Serial.println("Digite qual compartimento deseja programar:(1 a 21)\n");
   while (Serial.available() == 0){ delay(10); }
   medsProgramCalender[medsProgramTotalCount].slot = Serial.parseInt();
   while (Serial.available() > 0) {
     Serial.read();
   }
-
+  
   Serial.println("digite a hora do remedio:\n");
   while (Serial.available() == 0){ delay(10); }
   medsProgramCalender[medsProgramTotalCount].hour = Serial.parseInt();
   while (Serial.available() > 0) {
   Serial.read();
+}
+
+Serial.printf("digite os minutos:\n");
+while (Serial.available() == 0){ delay(10); }
+medsProgramCalender[medsProgramTotalCount].minutes = Serial.parseInt();
+while (Serial.available() > 0) {
+    Serial.read();
   }
   
-  Serial.printf("digite os minutos:\n");
-  while (Serial.available() == 0){ delay(10); }
-  medsProgramCalender[medsProgramTotalCount].minutes = Serial.parseInt();
-  while (Serial.available() > 0) {
-  Serial.read();
-  }
-
   Serial.println("digite o dia do remedio:\n");
   while (Serial.available() == 0){ delay(10); }
   medsProgramCalender[medsProgramTotalCount].weekDay = Serial.parseInt();
   while (Serial.available() > 0) {
     Serial.read();
   }
-
+  
   medsProgramTotalCount ++;
   Serial.printf("programacao salva: Slot %02d, Horario %02d:%02d, Dia %02d.\n", medsProgramTotalCount, medsProgramCalender[medsProgramTotalCount].hour, medsProgramCalender[medsProgramTotalCount].minutes, medsProgramCalender[medsProgramTotalCount].weekDay);
-
+  
 }
 
-
-int findTodayPrograms(int* foudedProgramsFoToday){
-   
-  struct tm currentTime;
+int findTodaysPrograms(int* foundedProgram){
+  
   if (!getLocalTime(&currentTime)) {
     delay(1000);
     return 0;
   }
-
-  int foundedProgramsForToday[3]; 
+  
   int counter = 0;
-
+  
   for(int programNumber = 0; programNumber < 21; programNumber++){
     if(currentTime.tm_wday == medsProgramCalender[programNumber].weekDay){
-      foundedProgramsForToday[counter] = programNumber;
+      foundedProgram[counter] = programNumber;
       counter ++;
     }
   }
-
+  
   return counter;
 }
 
-
-void setup() {
-  Serial.begin(115200);
+int findSlotsForThisHour(int* todaysPrograms){
   
-  WiFi.mode(WIFI_STA);
-  WiFi.disconnect();
-  delay(100);
-  WiFi.begin(ssid, password);
-  Serial.print("connecting to wifi");
-  while (WiFi.status() != WL_CONNECTED) {
-      delay(500);
-      Serial.print(".");
+  if(!getLocalTime(&currentTime)){
+    Serial.printf("erro na hora\n");
   }
-  Serial.println("\nWifi -> OK");
-  configTime(brasiliaTime, summerTimeOffset, ntpServer);
   
-  
-  myStepper.setSpeed(5);
-
-  makeNewProgram();
-  
+  for(int i = 0; i < 3; i++){
+    if (currentTime.tm_hour == medsProgramCalender[todaysPrograms[i]].hour && currentTime.tm_min == medsProgramCalender[todaysPrograms[i]].minutes   ){
+      return todaysPrograms[i];
+    }
+  }
+  return -1;
 }
 
-const char* ssid = "******"; 
-const char* password = "*****";
+
+const int stepsPerRevolution = 2048;
+int currentSlot = 0;
+#define IN1 19
+#define IN2 18
+#define IN3 5
+#define IN4 17
+
+Stepper myStepper(stepsPerRevolution, IN1, IN3, IN2, IN4);
+
+void rotateToSlot(int targetSlot){
+  
+  Serial.printf("\n--- INICIANDO ENTREGA ---\n");
+  Serial.printf("Carrossel atual: Gaveta %d | Destino: Gaveta %d\n", currentSlot, targetSlot);
+
+  float currentAngle = storageCarouselSlots[currentSlot];
+  float targetAngle = storageCarouselSlots[targetSlot];
+
+  float differenceBetweenCurrentAndTargetAngle = targetAngle - currentAngle;
+
+  long stepsToWalk = (differenceBetweenCurrentAndTargetAngle / 360.0) * stepsPerRevolution;
+
+  Serial.printf("Girando o motor em %ld passos...\n", stepsToWalk);
+
+  myStepper.step(stepsToWalk);
+
+  Serial.println(">>> REMEDIO DISPENSADO COM SUCESSO! <<<");
+
+  currentSlot = targetSlot;
+
+  return;
+}
+
+const char* ssid = "iPhone de Guilherme Andrade"; 
+const char* password = "flaamer123";
 const char* ntpServer = "pool.ntp.org";
 const long  brasiliaTime = -3 * 3600;
 const int   summerTimeOffset = 0;
 
+void setup() {
+  Serial.begin(115200);
+  
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("\nWifi -> OK");
+  configTime(brasiliaTime, summerTimeOffset, ntpServer);
+  
+  myStepper.setSpeed(5);
+  
+  makeNewProgram();
+  
+}
+
+int lastVerifiedMinute = -1;
+
 void loop() {
-  struct tm currentTime;
+
   if(!getLocalTime(&currentTime)){
     return;
   }
 
   Serial.printf("%02d:%02d:%02d\n", currentTime.tm_hour, currentTime.tm_min, currentTime.tm_sec);
 
-  if(HaveProgramsForToday){
+  if (currentTime.tm_min % 15 == 0 && currentTime.tm_min != lastVerifiedMinute && thereAreProgramsForToday()) {
 
-    if(isSunday){
-
-    }else {
-      // aqui vai ter uma parte de notificação 
+    if(thereAreProgramsForToday()){
+  
+      if(isSunday()){
+        // aqui vai ter uma parte de notificação 
+      }
+      
+      int todaysPrograms[3];
+      int quantityOfProgramsForToday = findTodaysPrograms(todaysPrograms);
+      
+      Serial.printf("Os remadios para hoje sao:\n");
+      for(int i = 0; i < 3; i++){
+        Serial.printf(" Slot %02d;\n Horario %02d:%02d;\n Dia %02d.\n", medsProgramCalender[todaysPrograms[i]].slot, medsProgramCalender[todaysPrograms[i]].hour, medsProgramCalender[todaysPrograms[i]].minutes, medsProgramCalender[todaysPrograms[i]].weekDay);
+      }
+  
+      if (isTimeForMeds()){
+        int slotForThisHour = findSlotsForThisHour(todaysPrograms);
+        rotateToSlot(slotForThisHour);
+      }
+  
+    }else{
+      delay(10000);
+      // alguma coisa pra ele dormir aumentar o delay do loop para ele não ficar atualizando de 15 em 15 min
     }
-    
-    int todayPrograms[3];
-    int programQuantityForToday = findTodayPrograms(todayPrograms);
-    
-    Serial.printf("Os remadios para hoje sao:\n");
-    for(int i = 0; i < 3; i++){
-      Serial.printf(" Slot %02d;\n Horario %02d:%02d;\n Dia %02d.\n", medsProgramCalender[todayPrograms[i]].slot, medsProgramCalender[todayPrograms[i]].hour, medsProgramCalender[todayPrograms[i]].minutes, medsProgramCalender[todayPrograms[i]].weekDay);
-    }
 
-    isTimeForMeds();
-
-  }else{
-    delay(10000);
-    // alguma coisa pra ele dormir aumentar o delay do loop para ele não ficar atualizando de 15 em 15 min
+    lastVerifiedMinute = currentTime.tm_min;
   }
 
 
